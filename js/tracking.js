39297a82f5ee53553a615dbdedf756ff861f5a97
Matt Williams <mwilliamsii233203@gmail.com>
2020-01-03 12:23:43 -0500

    Add files via upload

--- js/tracking.js
+++ js/tracking.js
@@ -14,6 +14,7 @@
         firebase.initializeApp(firebaseConfig);
         firebase.auth().onAuthStateChanged((user) => {
             if(user){
+                let i = 0;
                 let userUid = user.uid
                 firebase.database().ref("Admin/MensEntries/").on("child_added", (snapshot) => {
                     let data = snapshot.val();
@@ -44,7 +45,7 @@
                         
                         let name = document.createElement("h5");
                         name.classList.add("col");
-                        name.classList.add("m9");
+                        name.classList.add("m7");
                         $(name).css("font-weight", "600");
                         // name.classList.add("center-align")
         
@@ -54,6 +55,38 @@
                         });
         
                         div.appendChild(name);
+
+                        let a = document.createElement("a");
+                        a.textContent = "View Teams";
+                        a.href = "#modal" + i;
+                        a.classList.add("col");
+                        a.classList.add("m2");
+                        a.classList.add("teamViewBtn");
+                        a.classList.add("modal-trigger");
+                        div.appendChild(a);
+
+                        let modalDiv = document.createElement("div");
+                        modalDiv.classList.add("modal");
+                        modalDiv.id = "modal" + i;
+                        div.appendChild(modalDiv);
+
+                        let modalContentDiv = document.createElement("div");
+                        modalContentDiv.classList.add("modal-content");
+                        modalDiv.appendChild(modalContentDiv);
+
+                        let modalTitle = document.createElement("h4");
+                        modalTitle.textContent = "Chosen Teams: ";
+                        modalContentDiv.appendChild(modalTitle);
+
+                        for(let i = 0; i < dataTwo.MensEntry.length; i++){
+                            let p = document.createElement("p");
+                            p.textContent = dataTwo.MensEntry[i] + " ";
+                            p.classList.add("col");
+                            p.classList.add("m3");
+                            p.classList.add("team");
+                            p.classList.add("center-align")
+                            modalContentDiv.appendChild(p);
+                        }
         
                         let score = document.createElement("h5");
                         score.classList.add("col");
@@ -70,21 +103,11 @@
                         hr.classList.add("offset-m1");
                         div.appendChild(hr);
 
-                        let teamDiv = document.createElement("div");
-                        teamDiv.classList.add("col");
-                        teamDiv.classList.add("m12");
-                        teamDiv.classList.add("teamList");
-                        div.appendChild(teamDiv);
-
-                        for(let i = 0; i < dataTwo.MensEntry.length; i++){
-                            let p = document.createElement("p");
-                            p.textContent = dataTwo.MensEntry[i] + " ";
-                            p.classList.add("col");
-                            p.classList.add("m3");
-                            p.classList.add("team");
-                            p.classList.add("center-align")
-                            teamDiv.appendChild(p);
-                        }
+                        // let teamDiv = document.createElement("div");
+                        // teamDiv.classList.add("col");
+                        // teamDiv.classList.add("m12");
+                        // teamDiv.classList.add("teamList");
+                        // div.appendChild(teamDiv);
         
                         $("#mens").append(div);
                         
@@ -110,12 +133,23 @@
                         });
                         // finally append the sorted elements again, the old element will be moved to
                         // the new position
+                        let x = 1;
                         for (let i = 0; i < sortMe.length; i++) {
                             // remember that the second array element contains the element itself
                             let classSplit = sortMe[i][1].className.split("col m12 ");
-                            document.querySelector("." + classSplit[1] + " .place").textContent = (i + 1);
+                            if(i != 0){
+                                if(sortMe[i - 1][1].id == sortMe[i][1].id){
+                                    document.querySelector("." + classSplit[1] + " .place").textContent = x;
+                                }else{
+                                    x++;
+                                    document.querySelector("." + classSplit[1] + " .place").textContent = x;
+                                }
+                            }
                             container.appendChild(sortMe[i][1]);
                         }
+
+                        i++;
+                        $('.modal').modal();
                     });
                 });
 
@@ -150,7 +184,7 @@
                         
                         let name = document.createElement("h5");
                         name.classList.add("col");
-                        name.classList.add("m9");
+                        name.classList.add("m7");
                         $(name).css("font-weight", "600");
                         // name.classList.add("center-align")
         
@@ -160,6 +194,38 @@
                         });
         
                         div.appendChild(name);
+
+                        let a = document.createElement("a");
+                        a.textContent = "View Teams";
+                        a.href = "#modalw" + i;
+                        a.classList.add("col");
+                        a.classList.add("m2");
+                        a.classList.add("teamViewBtn");
+                        a.classList.add("modal-trigger");
+                        div.appendChild(a);
+
+                        let modalDiv = document.createElement("div");
+                        modalDiv.classList.add("modal");
+                        modalDiv.id = "modalw" + i;
+                        div.appendChild(modalDiv);
+
+                        let modalContentDiv = document.createElement("div");
+                        modalContentDiv.classList.add("modal-content");
+                        modalDiv.appendChild(modalContentDiv);
+
+                        let modalTitle = document.createElement("h4");
+                        modalTitle.textContent = "Chosen Teams: ";
+                        modalContentDiv.appendChild(modalTitle);
+
+                        for(let i = 0; i < dataTwo.WomensEntry.length; i++){
+                            let p = document.createElement("p");
+                            p.textContent = dataTwo.WomensEntry[i] + " ";
+                            p.classList.add("col");
+                            p.classList.add("m3");
+                            p.classList.add("team");
+                            p.classList.add("center-align")
+                            modalContentDiv.appendChild(p);
+                        }
         
                         let score = document.createElement("h5");
                         score.classList.add("col");
@@ -176,21 +242,21 @@
                         hr.classList.add("offset-m1");
                         div.appendChild(hr);
 
-                        let teamDiv = document.createElement("div");
-                        teamDiv.classList.add("col");
-                        teamDiv.classList.add("m12");
-                        teamDiv.classList.add("teamList");
-                        div.appendChild(teamDiv);
+                        // let teamDiv = document.createElement("div");
+                        // teamDiv.classList.add("col");
+                        // teamDiv.classList.add("m12");
+                        // teamDiv.classList.add("teamList");
+                        // div.appendChild(teamDiv);
 
-                        for(let i = 0; i < dataTwo.WomensEntry.length; i++){
-                            let p = document.createElement("p");
-                            p.textContent = dataTwo.WomensEntry[i] + " ";
-                            p.classList.add("col");
-                            p.classList.add("m3");
-                            p.classList.add("team");
-                            p.classList.add("center-align")
-                            teamDiv.appendChild(p);
-                        }
+                        // for(let i = 0; i < dataTwo.WomensEntry.length; i++){
+                        //     let p = document.createElement("p");
+                        //     p.textContent = dataTwo.WomensEntry[i] + " ";
+                        //     p.classList.add("col");
+                        //     p.classList.add("m3");
+                        //     p.classList.add("team");
+                        //     p.classList.add("center-align")
+                        //     teamDiv.appendChild(p);
+                        // }
         
                         $("#womens").append(div);
                         
@@ -229,6 +295,7 @@
 
         $(".tabs").tabs();
         $("#logout").on("click", logout);
+        $('.modal').modal();
     }
 
     function logout(){
